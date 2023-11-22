09e67d02d29a12be003342caa8221fa5f0c50598
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2023-11-22 18:36:26 +0200

    added Name and Link properties

--- FollowerParser/MVVM/Model/Follower.cs
+++ FollowerParser/MVVM/Model/Follower.cs
@@ -9,6 +9,8 @@ namespace FollowerParser.MVVM.Model
     internal class Follower
     {
         public string UserName { get; set; }
+        public string? Name { get; set; }
         public string? Bio { get; set; }
+        public string? Link { get; set; }
     }
 }
